977ebc9c63e9131fa316e5313cb008d3bc72dcb8
azeemrh16 <azeem.rahul16@gmail.com>
2020-10-21 07:41:41 +0100

    volumes

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -4,10 +4,13 @@ set -ex
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
-kubectl apply -f deployment/pages-deployment.yaml
-
+kubectl apply -f deployment/mysql-storage-class.yaml
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
 kubectl apply -f deployment/mysql-secret.yaml
 kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
+sleep 20
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+kubectl apply -f deployment/pages-deployment.yaml
